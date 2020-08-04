ec916cab9982f546c105624f7943ed9c9f8b3294
Mattias Strömberg <m.stromberg@outlook.com>
2020-08-04 17:09:22 +0200

    Your first code
    Your first code

---
+++ HelloWorld/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace HelloWorld
+{
+    public class HelloWorld : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+            print("Hello, World!");
+        }
+    }
+}
